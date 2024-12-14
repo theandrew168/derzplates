579543c4f27477a43925b5d98edd19b2e722824d
theandrew168 <andrew@shallowbrooksoftware.com>
2024-12-13 23:20:00 -0600

    Still trying to find a reset method that works

--- DerzPlates/main.lua
+++ DerzPlates/main.lua
@@ -5,11 +5,10 @@
 -- https://wowwiki-archive.fandom.com/wiki/Events_A-Z_(full_list)
 -- https://cdn.wowinterface.com/forums/showthread.php?t=36061
 -- https://www.wowinterface.com/forums/showthread.php?t=34248
+-- https://wowpedia.fandom.com/wiki/API_UnitReaction
 -- https://wowpedia.fandom.com/wiki/API_UnitThreatSituation
 -- https://wowpedia.fandom.com/wiki/API_UnitDetailedThreatSituation
--- https://github.com/Gethe/wow-ui-source/blob/10.2.0/Interface/AddOns/Blizzard_NamePlates/Blizzard_NamePlates.lua
--- https://wowpedia.fandom.com/wiki/Secure_Execution_and_Tainting
--- https://github.com/Pondidum/WowInterfakes/blob/master/wow-ui-source/FrameXML/CompactUnitFrame.lua
+-- https://github.com/Gethe/wow-ui-source/blob/classic_era/Interface/AddOns/Blizzard_NamePlates/Vanilla/Blizzard_NamePlates.lua-- https://wowpedia.fandom.com/wiki/Secure_Execution_and_Tainting
 
 local statusText = {
 	[0] = "(0) low on threat",
@@ -17,23 +16,6 @@ local statusText = {
 	[2] = "(2) primary target but not on high threat",
 	[3] = "(3) primary target and high threat",
 }
-
-hooksecurefunc("CompactUnitFrame_UpdateHealthColor", function(frame)
-	-- Check if the player is currently tanking the name plate's unit.
-	local status = UnitThreatSituation("player", frame.unit)
-
-	local name, _ = UnitName(frame.unit)
-	if status ~= nil then
-		print(name, "is", statusText[status])
-	end
-
-	if status == 2 or status == 3 then
-		-- If they ARE tanking this unit, set the plate's color to magenta.
-		frame.healthBar:SetStatusBarColor(1, 0, 1, 1)
-	else
-		print("not tanking, leaving default:", name)
-	end
-end)
 
 -- Create the addon "frame" object for handling events.
 local frame = CreateFrame("Frame")
@@ -49,9 +31,18 @@ frame:RegisterEvent("COMBAT_LOG_EVENT")
 
 -- When combat happens, run the main addon loop.
 frame:SetScript("OnEvent", function(self, event, frame)
+	NamePlateDriverFrame.UpdateAllHealthColor()
+	print("frame", NamePlateDriverFrame)
+	print("dot", NamePlateDriverFrame.UpdateAllHealthColor)
+	-- print("colon", NamePlateDriverFrame:UpdateAllHealthColor)
+	-- print("method", NamePlateDriverFrame:UpdateAllHealthColor)
 	-- Iterate over all currently-visible name plates.
 	for i, namePlate in ipairs(C_NamePlate.GetNamePlates()) do
 		local unitFrame = namePlate.UnitFrame
+
+		-- Check if the name plate's unit is neutral (needed for default colors).
+		local reaction = UnitReaction(unitFrame.unit, "player")
+		local isNeutral = reaction == 4
 
 		-- Check if the player is currently tanking the name plate's unit.
 		local status = UnitThreatSituation("player", unitFrame.unit)
@@ -64,6 +55,12 @@ frame:SetScript("OnEvent", function(self, event, frame)
 		if status == 2 or status == 3 then
 			-- If they ARE tanking this unit, set the plate's color to magenta.
 			unitFrame.healthBar:SetStatusBarColor(1, 0, 1, 1)
+		elseif isNeutral then
+			-- Otherwise, if the unit is neutral, set the plate's color to yellow.
+			unitFrame.healthBar:SetStatusBarColor(1, 1, 0, 1)
+		else
+			-- Lastly, default the plate's color to red.
+			unitFrame.healthBar:SetStatusBarColor(1, 0, 0, 1)
 		end
 	end
 end)
